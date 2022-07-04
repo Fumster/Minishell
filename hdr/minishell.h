b0af6f7c2c93b516dd3e80bd9cbd637caec089df
Freely Chrystal <fchrysta@student.21-school.ru>
2022-07-04 20:14:26 +0300

    fix typos

--- hdr/minishell.h
+++ hdr/minishell.h
@@ -6,11 +6,11 @@
 /*   By: fchrysta <fchrysta@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 20:36:04 by fchrysta          #+#    #+#             */
-/*   Updated: 2022/07/03 17:53:19 by fchrysta         ###   ########.fr       */
+/*   Updated: 2022/07/04 20:13:06 by fchrysta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef		MINISHELL_H
-# define	MINISHELL_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 # include"libft.h"
 # include<stdio.h>
 # include<stdlib.h>
@@ -19,13 +19,13 @@
 # include<readline/readline.h>
 # include<readline/history.h>
 
-typedef struct	s_global
+typedef struct s_global
 {
 	int		exitcode;
 }			t_global;
 
-extern t_global g_global;
+extern t_global	g_global;
 
-void	init_sighandler();
+void	init_sighandler(void);
 
 #endif
